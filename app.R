17e7465735e9cd8312af8f8e315fd2a73a37b5ef
indra wiryanto <indrarencongsakti33@gmail.com>
2023-06-06 18:34:20 +0700

    update 2, 3

--- app.R
+++ app.R
@@ -8,7 +8,48 @@ library(DT)
 library(echarts4r) 
 
 data <- read_csv("data_pers_kri_rev1.csv")
-view(data)
+data1 <- read_csv("data_pers_kri_rev1.csv")
+regulation <- read_csv2(file = "REGULATION.csv")
+
+
+
+jabatan_pred_function <- function(data, nrp) {
+  # regulation
+  regulation %>% 
+    separate(KEN_KAT, into = c("pkt_now", "pkt_next"), sep = "Ke") %>%
+    mutate(
+      pkt_now = trimws(pkt_now),
+      pkt_next = trimws(pkt_next)
+    ) %>% 
+    rename(MDP_syarat = MDP, MDDP_syarat = MDDP) -> regulation_final
+  
+  data %>% 
+    filter(NRP == nrp) -> one_person_data
+  
+  regulation_final %>% 
+    filter(
+      pkt_now == trimws(one_person_data$PKT_RIIL)
+    ) -> filtered_jabatan
+  
+  if (!any(is.na(filtered_jabatan$DIK_BANG))) {
+    filtered_jabatan %>% 
+      filter(str_detect(DIK_BANG, trimws(one_person_data$DIKBANG))) %>% 
+      select(pkt_now, pkt_next,DIK_BANG, MDP_syarat) -> filtered_jabatan
+    one_person_data %>% 
+      bind_cols(filtered_jabatan) %>% 
+      mutate(bisa_naik_pangkat = ifelse(MDP >= MDP_syarat, "Ya", "Tidak")) -> result
+  } else {
+    
+    filtered_jabatan %>% 
+      select(pkt_now, pkt_next,DIK_BANG, MDDP_syarat) -> filtered_jabatan
+    
+    one_person_data %>% 
+      bind_cols(filtered_jabatan) %>% 
+      mutate(bisa_naik_pangkat = ifelse(MDDP >= MDDP_syarat, "Ya", "Tidak")) -> result
+    
+  }
+  return(result)
+}
 
 ui <- dashboardPage(
   dashboardHeader(
@@ -18,8 +59,8 @@ ui <- dashboardPage(
     sidebarMenu(
       id = "tabs",
       menuItem("Main Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Procesing Data", tabName = "emp", icon = icon("users")),
-      menuItem("Predictive Pers", tabName = "empp", icon = icon("user"))
+      menuItem("Procesing Data", tabName = "profile", icon = icon("users")),
+      menuItem("Predictive Pers", tabName = "prediksi", icon = icon("user"))
     )
   ),
   dashboardBody(
@@ -43,12 +84,67 @@ ui <- dashboardPage(
               )
       ),
       tabItem(
-        tabName = "emp",
-        h1("Attrition Employee Dashboard"),
+        
+        tabName = "profile",
+        h1("Profile Personel"),
+        
+        selectInput(inputId = "profile", label = "NRP", choices = data1$NRP),
+        actionButton(inputId = "Btn", label = "search", style= "margin-bottom:14px", icon = icon("magnifying-glass")),
+        
+        uiOutput("coba"),
+        fluidRow( #img(src = "images/pngwing.com.png"),
+          
+          valueBoxOutput("personalInformation", width = 6),
+          valueBoxOutput("pangkat", width = 6),
+          
+        ),
+        
+        box(width = 6,
+            h3("Personal Information"),
+            textOutput(outputId = "tgl"),
+            textOutput(outputId = "usia"),
+            textOutput(outputId = "tmt"),
+            textOutput(outputId = "edufild")),
+        
+        box(width = 6,
+            h3("DATA DUKUNG"),
+            textOutput(outputId = "dapen"),
+            textOutput(outputId = "urikes"),
+            textOutput(outputId = "samapta"),
+            textOutput(outputId = "bing")),
+        
+        
+      
       ),
       tabItem(
-        tabName = "empp",
-        h1("Employ Profile"),
+        
+        tabName = "prediksi",
+        h1("Prediksi"),
+        fluidRow(
+          box(width = 6,
+              selectInput(inputId = "predict", label = "Masukan Nrp", choices = data1$NRP),
+              box(
+                textOutput(outputId = "nama"),
+                textOutput(outputId = "kat"),
+                textOutput(outputId = "korp"),
+                textOutput(outputId = "mdp"),
+                textOutput(outputId = "mddp"),
+                textOutput(outputId = "prediksi_usia")
+                
+              ),
+              actionButton(inputId = "btn2", label = "Prediksi", icon = icon("magnifying-glass")))
+        ),
+        
+        
+        box(width = 3, h2("Prediksi Jabatan"),
+            textOutput(outputId = "predJab")),
+        
+        box(width = 3, h2("Prediksi Kenkat"),
+            textOutput(outputId = "predKenkat")),
+        
+        box(width = 3, h1("Prediksi Sekolah"),
+            textOutput(outputId = "predSekolah"))
+      
         
       )
     )
@@ -144,6 +240,174 @@ server <- function(input, output, session) {
     req(input$Btn)
     paste0("ID Employee: ", input$profile)
   })
+  
+  filterData <- eventReactive(input$Btn,{
+    data1 %>% filter(NRP == input$profile)
+  })
+  
+  output$personalInformation <- renderValueBox({
+    req(filterData())
+    data_personalInformation<- filterData()%>%
+      select(NAMA)
+    
+    valueBox(value= data_personalInformation$NAMA , subtitle = "nama", icon = icon("user-times"), color = "red")
+  })
+  
+  ########################################################################
+  
+  output$pangkat <- renderValueBox({
+    req(filterData())
+    data_pangkat <-  filterData()%>%
+      select(PKT_RIIL)
+    
+    
+    valueBox(value= data_pangkat$PKT_RIIL,  subtitle = "PANGKAT", icon = icon("id-badge"))
+  })
+  ##################################################
+  
+  
+  output$tgl <- renderText({
+    paste0("1. Tanggal Lahir: ", filterData()$TGL_LAHIR)
+  })
+  
+  output$usia <- renderText({
+    paste0("2. Usia: ", filterData()$USIA_RIIL)
+  })
+  output$tmt <- renderText({
+    paste0("3. TMT TNI: ", filterData()$TMT_TNI)
+  })
+  
+  
+  ####################################### 
+  output$dapen <- renderText({
+    paste0("1. Dapen : ", filterData()$DAPEN)
+  })
+  
+  output$urikes <- renderText({
+    paste0("2. Urikes: ", filterData()$URIKES)
+  })
+  
+  output$samapta <- renderText({
+    paste0("3. Samapta :  ", filterData()$SAMAPTA)
+  })
+  
+  output$bing <- renderText({
+    paste0("4. B. Inggris : ", filterData()$BHS.INGGRIS)
+  })
+  
+  
+  output$satis <- renderText({
+    paste0("Environment Satisfaction: ", filterData()$EnvironmentSatisfaction)
+  })
+  
+  
+  output$jobsatis <- renderText({
+    paste0("Job Satisfaction: ", filterData()$JobSatisfaction)
+  })
+  
+  output$relation <- renderText({
+    paste0("Relationship Satisfaction: ", filterData()$RelationshipSatisfaction)
+  })
+  
+  output$work <- renderText({
+    paste0("Work Life Balance: ", filterData()$WorkLifeBalance)
+  })
+  
+  ###########################################################################################
+  filterData1 <- eventReactive(input$btn2,{
+    data1 %>% jabatan_pred_function(nrp = input$predict)
+  })
+  
+  
+  output$nama <- renderText({
+    req(filterData1())
+    paste0("Nama : ", filterData1()$NAMA)
+  })
+  
+  output$kat <- renderText({
+    req(filterData1())
+    paste0("Pangkat : ", filterData1()$PKT_RIIL)
+  })
+  
+  
+  output$korp <- renderText({
+    req(filterData1())
+    paste0("Korps : ", filterData1()$KORPS_RIIL)
+  })
+  
+  output$mdp <- renderText({
+    req(filterData1())
+    paste0("MDP : ", filterData1()$MDP)
+  })
+  
+  output$mddp <- renderText({
+    req(filterData1())
+    paste0("MDDP : ", filterData1()$MDDP)
+  })
+  
+  output$prediksi_usia <- renderText({
+    req(filterData1())
+    paste0("Usia : ", filterData1()$USIA_RIIL)
+  })
+  
+  
+  output$predJab <- renderText({
+    req(filterData1())
+    paste0("Jabatan :", filterData1()$pkt_next)
+  })
+  
+  output$predKenkat <- renderText({
+    req(filterData1())
+    paste0("Kenkat :", filterData1()$bisa_naik_pangkat)
+  })
+  
+  output$predSekolah <- renderText({
+    req(filterData1())
+    pkt_now <- filterData1()$pkt_now
+    MDDP <- filterData1()$MDDP
+    MDP <- filterData1()$MDP
+    USIA_RIIL <- filterData1()$USIA_RIIL
+    DIKBANG <- filterData1()$DIKBANG
+    
+    if (pkt_now == "SERMA" & MDDP >=0 & USIA_RIIL >= 34) {
+      sekolah <- "DIKTUKPA"
+    } else if (pkt_now == "PELTU" & MDDP >= 0 & USIA_RIIL >= 45) {
+      sekolah <- "DIKTUPAKAT"
+    } else if (pkt_now == "KOPDA" & MDDP >= 2 & USIA_RIIL >= 31) {
+      sekolah <- "DIKTUKBA"
+    } else if(pkt_now == "KOPKA" & USIA_RIIL >= 41) {
+      sekolah <- "DIKTUKBAKAT"
+    } else if(pkt_now == "KAPTEN" & MDDP >= 2 & USIA_RIIL <= 41 & DIKBANG == "DIKSPESPA") {
+      sekolah <- "DIKLAPA"
+    } else if (pkt_now == "MAYOR" & MDDP >= 0 & USIA_RIIL <= 41 & DIKBANG == "DIKSPESPA") {
+      sekolah <- "DIKLAPA"
+    } else if (pkt_now == "KAPTEN" & MDDP >= 2 & USIA_RIIL <= 41 & DIKBANG == "DIKSPESPA") {
+      sekolah <- "DIKMATRA 2/DIKAPLIKASI 2"
+    } else if (pkt_now == "MAYOR" & MDDP >= 0 & USIA_RIIL <= 41 & DIKBANG == "DIKSPESPA") {
+      sekolah <- "DIKMATRA 2/DIKAPLIKASI 2"
+    } else if (pkt_now == "MAYOR" & MDDP >= 2 & USIA_RIIL <= 48 & DIKBANG == "DIKLAPA"){
+      sekolah <- "SESKO ANGKATAN"
+    } else if (pkt_now == "LETKOL" & MDDP >= 0 & USIA_RIIL <= 48 & DIKBANG == "DIKLAPA") {
+      sekolah <- "SESKO ANGKATAN"
+    } else if (pkt_now == "MAYOR" & MDDP >= 2 & USIA_RIIL <= 45 & DIKBANG == "DIKLAPA") {
+      sekolah <- "DIKAPLIKASI 3"
+    } else if (pkt_now == "LETKOL" & MDDP >= 0 & USIA_RIIL <= 45 & DIKBANG == "DIKLAPA") {
+      sekolah <- "DIKAPLIKASI 3"
+    } else if (pkt_now == "KOLONEL" & MDDP >= 2 & USIA_RIIL <= 49 & DIKBANG == "SESKO ANGKATAN") {
+      sekolah <- "SESKO TNI"
+    } else if (pkt_now == "KOLONEL" & MDP >= 24 & USIA_RIIL <= 53 & DIKBANG == "SESKO TNI") {
+      sekolah <- "LEMHANAS PPRA"
+    } else if (pkt_now == "LAKSAMANA PERTAMA" & MDDP >= 0 & USIA_RIIL <= 53 & DIKBANG == "SESKO TNI") {
+      sekolah <- "LEMHANAS PPRA"
+    } else {
+      sekolah <- "Tidak Boleh Sekolah"
+    }
+    
+    
+    sekolah
+    paste0("Sekolah :", sekolah)
+  })
+  
 }
 
 shinyApp(ui, server)
